ae2be0ebd42c4844d4037216ada8b6273d458dea
HPHAN1 <huyphan@rocketmortgage.com>
2021-11-03 15:09:04 -0400

    2nd commit changed some stuff

--- Program.cs
+++ Program.cs
@@ -5,9 +5,10 @@ namespace BranchingPractice
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {            
+            Console.WriteLine("Adding and rearranging");
             Console.WriteLine("Hello James!");
+            Console.WriteLine("Hello World!");
             Console.WriteLine("Hello Huy! It is 2:53PM here in Windsor Ontario, what time is it in Michigan?");
         }
     }
